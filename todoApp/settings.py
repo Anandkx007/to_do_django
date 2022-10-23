092ec1c487d70306f17e4fdb2c76f24afda90634
Anandkx007 <Anandkx007@gmail.com>
2022-10-23 15:26:22 +0530

    first commit

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -125,6 +125,6 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
